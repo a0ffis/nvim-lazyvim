bcd74bea5e403f00764beeb73d5a83993eef2d40
nattapos <nattapos@scgexpress.co.th>
2024-01-12 19:00:15 +0700

    update: test plugin toggle with windows and mac to open and powershell and zsh

--- lua/plugins/toggle.lua
+++ lua/plugins/toggle.lua
@@ -1,25 +1,26 @@
 return {
-  "akinsho/toggleterm.nvim",
-  version = "*",
-  opts = {
-    size = 20,
-    open_mapping = [[<c-t>]],
-    hide_numbers = true,
-    shade_filetypes = {},
-    shade_factor = 2,
-    start_in_insert = true,
-    insert_mapping = true,
-    persist_size = true,
-    direction = "horizontal",
-    close_on_exit = true,
-    shell = vim.o.shell,
-    float_opts = {
-      border = "curved",
-      winblend = 0,
-      higlights = {
-        border = "Normal",
-        background = "Normal",
-      },
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    opts = {
+        size = 20,
+        open_mapping = [[<c-t>]],
+        hide_numbers = true,
+        shade_filetypes = {},
+        shade_factor = 2,
+        start_in_insert = true,
+        insert_mapping = true,
+        persist_size = true,
+        direction = "horizontal",
+        close_on_exit = true,
+        -- shell = vim.o.shell,
+        shell = vim.fn.executable("pwsh") and "pwsh" or "powershell" or "terminal_emulator",
+        float_opts = {
+            border = "curved",
+            winblend = 0,
+            higlights = {
+                border = "Normal",
+                background = "Normal",
+            },
+        },
     },
-  },
 }
